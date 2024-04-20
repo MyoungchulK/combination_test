10c348b4c44a9fd0757355a2785038e90438ce92
MyoungchulK <kmc8907@gmail.com>
2024-04-20 10:45:56 +0900

    solved by not satisfied...

--- com_test.py
+++ com_test.py
@@ -2,7 +2,8 @@ import numpy as np
 from itertools import combinations
 
 arr = np.sort(np.random.choice(np.arange(1, 20), size = 5, replace = False))
-#arr = np.array([ 5,  9, 10, 11, 16], dtype = int) # for test
+#arr = np.array([5,  9, 10, 11, 16], dtype = int) # for test
+arr = np.array([1,  3,  8, 14, 15], dtype = int) # for test
 print('ori arr:', arr)
 
 def func(target, thres = 3):
@@ -34,14 +35,21 @@ def func(target, thres = 3):
 
     ## find the actual numbers that make the survived combination
     nums = []
-    counts = np.full((tr_pat_len), 0, dtype = int)
+    counts = []
     for c in range(tr_pat_len):
         num_per_pat = np.unique(tg_coms[diff_bool[:, c]])
-        nums.append(num_per_pat)
-        counts[c] = len(num_per_pat)
+        zero_check = num_per_pat % 3 == 0
+        if np.count_nonzero(zero_check) > 1: # not satisfying soultion...
+            zero_val = num_per_pat[zero_check]
+            for z in range(len(zero_val)):
+                num_per_pat_v2 = np.append(num_per_pat[~zero_check], zero_val[z])
+                nums.append(num_per_pat_v2)
+                counts.append(len(num_per_pat_v2)) 
+        else:
+            nums.append(num_per_pat)
+            counts.append(len(num_per_pat))
     print('numbers that found by combination patterns:', nums)
-    max_idx = np.nanargmax(counts)
-    print(f'maximum size: {counts[max_idx]}, corresponding array: {nums[max_idx]}')
+    print(f'maximum size: {max(counts)}')
 
 
 func(arr)
